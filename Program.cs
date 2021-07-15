1154eeafd35250c1361ee0dbe76a93c9596d1d64
Will <doggy.huang@gmail.com>
2021-07-15 23:41:58 +0800

    PostAsJsonAsync 範例程式

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace TestPostAsJsonAsync
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var client = new HttpClient();
+
+            await client.PostAsJsonAsync(
+                "https://webhook.site/265de048-0b91-4d6b-be7d-16cbf209ff05",
+                new { ClientIP = "127.0.0.1" });
         }
     }
 }
